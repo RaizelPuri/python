e3b4b2ce893dfcc8f4d83dece5e5417b041f10a5
RaizelPuri <rosy.pearl2020@gmail.com>
2026-05-30 13:28:20 +0530

    Add files via upload

---
+++ print Statements and coments.py
@@ -0,0 +1,12 @@
+# printing a greeting message
+print("hello,world!")
+
+#printing my name 
+print("My name is RAIZEL.")
+
+#printing a fun fact about myself
+#print("I love painting")
+print(" I love painting.")
+
+#performing and printing a simple calculation
+print("The result of 3 + 5 is:" , 3 + 5)
