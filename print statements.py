ff3865d0ec1bda148facbb1ff2addc42add2b4ea
RaizelPuri <rosy.pearl2020@gmail.com>
2026-06-01 17:13:05 +0530

    Add files via upload

---
+++ print statements.py
@@ -0,0 +1,14 @@
+#print a statement
+print("Welcome to the world of programming!")
+
+#print a number
+print(4)
+
+#next line after print
+print("HELLO PYTHON /n")
+
+#pass multiple arguments for printing
+print("HELLO",5)
+
+#END ARGUMENT OF PRINT STATEMENT
+print("Welcome to", end="*")
