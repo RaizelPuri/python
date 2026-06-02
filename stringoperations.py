039405b5d102845360653eff815ad9be3747e8d4
RaizelPuri <rosy.pearl2020@gmail.com>
2026-06-02 16:40:11 +0530

    Add files via upload

---
+++ stringoperations.py
@@ -0,0 +1,9 @@
+text=str(input("enter a string"))
+
+#reverse string
+#using step value as -1 to ilerate in reverse
+revText=text[::-1]
+text=revText
+
+print(" reversed of given string is:")
+print(text)
