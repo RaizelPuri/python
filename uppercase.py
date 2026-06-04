ef73030f0ddf7d6bb00025948792c7ea3fa8b269
RaizelPuri <rosy.pearl2020@gmail.com>
2026-06-04 12:43:01 +0530

    Add files via upload

---
+++ uppercase.py
@@ -0,0 +1,12 @@
+
+text = input("Enter a string: ")
+
+print("Length of string:", len(text))
+
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+
+print("Count of 'a':", text.count('a'))
+
+print("Reversed string:", text[::-1])
+
