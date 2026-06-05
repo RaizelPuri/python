fefa4e638cf44d10add7b0766f35c8d5994fa0c2
RaizelPuri <rosy.pearl2020@gmail.com>
2026-06-05 16:32:52 +0530

    Add files via upload

---
+++ odd or even no.py
@@ -0,0 +1,8 @@
+number=int(input("ENTER NUMBER TO CHECK"))
+print("NUMBER TO BE CHECKED:",number)
+
+if number%2==0 :
+    print("THIS IS AN EVEN NUMBER")
+
+else:
+    print("THIS IS AN ODD NUMBER")
