d4cdea770fa30a29b79af3859fca89f0e4ec1af9
RaizelPuri <rosy.pearl2020@gmail.com>
2026-06-06 13:29:06 +0530

    Add files via upload

---
+++ Application of Logical not Operator in Python.py
@@ -0,0 +1,30 @@
+a=10
+b=12
+c=12
+
+#not is used here to reverse the result of(a == b)
+print(not(a == b))
+
+#not is used here to reverse the result of(b == c)
+print(not(b == c))
+
+a ="python" 
+b="coding"
+
+#not is used here to check that a is equal to b
+if not (a == b):
+    print (a, 'and', b, 'are different.')
+
+
+a=4
+b=5
+
+#not is used here to reverse the result of comparing both conditions
+if not ((a == 1) == (b == 5)):
+    print("hello")
+
+a=int(input("enter a number"))
+
+# not is used here to check that the number is not even
+if not (a % 2 == 0):
+    print(a, "is an odd number.")
