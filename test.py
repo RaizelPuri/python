ac1ed330b3a9959c0ecec60ed9578997f7cb82a2
RaizelPuri <rosy.pearl2020@gmail.com>
2026-07-10 18:17:58 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,39 @@
+print(" TRY TO GUESS A NUMBER IN JUST 5 ATTEMPTS")
+print("❤️❤️❤️❤️❤️")
+print("the number is between 1 - 50")
+secret=34
+print(int(input("ENTER A NUMBER: ")))
+
+if int==34:
+    print("YAYYY YOU GUESSED THE NUMBER")
+elif secret>10:
+    print("🧊 ice cold (very far)")
+    print("4 attempts left")
+    print("❤️❤️❤️❤️")
+    print(int(input("ENTER A NUMBER: ")))
+elif secret>20:
+    print("🥶 cold (far)")
+    print("3 attempts left")
+    print("❤️❤️❤️")
+    print(int(input("ENTER A NUMBER: ")))
+elif secret<30:
+    print("🌡️ warm (close)")
+    print("2 attempts left")
+    print("❤️❤️")
+    print(int(input("ENTER A NUMBER: ")))
+elif secret<40:
+    print("🥶 cold (far)")
+    print("1 attempts left")
+    print("❤️")
+    print(int(input("ENTER A NUMBER: ")))
+elif secret<30:
+    print("🔥 hot (very close)")
+    print("0 attempts left")
+else:
+    print("THE NUMBER WAS 34")
+
+
+
+
+
+ 
