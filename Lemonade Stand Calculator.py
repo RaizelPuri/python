adeb44b3d2e015cd01f890d8dba20a0d543bedf4
RaizelPuri <rosy.pearl2020@gmail.com>
2026-07-24 18:01:28 +0530

    Add files via upload

---
+++ Lemonade Stand Calculator.py
@@ -0,0 +1,43 @@
+def greet_customer():
+    print("Welcome to the Lemonade Stand!")
+    print("Fresh lemonade , made just for you.")
+
+greet_customer()
+
+price_per_cup = float(input("Enter the price per cup in dollars: "))
+cups_sold = int(input("Enter the number of cups sold: "))
+def calculate_total(price,cups):
+    total = price * cups
+    return total
+
+total_cost = calculate_total(price_per_cup, cups_sold)
+
+rounded_total= round(total_cost,2)
+print("TOTAL COST: ", rounded_total)
+
+amount_paid = float(input("Enter the amount paid by the customer:  "))
+
+def calculate_change(paid,total):
+    change = paid-total
+    return change
+
+change_due = calculate_change(amount_paid, rounded_total)
+rounded_change = round(change_due, 2)
+
+def thank_you_message(cups):
+    if cups>=5:
+        return "WOW,  big order!!  THANKS so much for your support!"
+    else:
+        return "THANKS for stopping by the stand!"
+
+closing_message = thank_you_message(cups_sold)
+
+print("")
+print("===== LEMONADE STAND RECEIPT =====")
+print("Price Per Cup: ", price_per_cup)
+print("Cups Sold: ", cups_sold)
+print("Total Cost: ",rounded_total)
+print("Amount Paid: ",amount_paid)
+print("Change Due: ",rounded_change)
+print(closing_message)
+print("=======================================")
